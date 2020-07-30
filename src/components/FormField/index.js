2758629c7b21828af054676e1b5048d98b7921c3
Yuri Cunha Coelho <yuriccoe@gmail.com>
2020-07-30 10:37:06 -0300

    Instalação Eslint

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -2,17 +2,17 @@ import React from "react";
 
 function FormField({ label, name, type, value, onChange }) {
     return(
-        <div>
-            <label>
-                {label}:
-                <input
-                    type={type}
-                    value={value}
-                    name={name}
-                    onChange={onChange}
-                    />
-            </label>
-        </div>
+    <div>
+        <label>
+            {label}:
+            <input
+                type={type}
+                value={value}
+                name={name}
+                onChange={onChange}
+                />
+        </label>
+    </div>
   )
 }
 
